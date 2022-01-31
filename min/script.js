cfdf2bf41ef6aaa4d8dbcbfdf4d31086f7b2cf5b
lyudmilaklochko <devonkalyu@gmail.com>
2022-01-31 17:40:20 +0200

    first commit

---
+++ min/script.js
@@ -0,0 +1,58 @@
+// function greet() {
+//     alert('Hello World!');  Function Declaration (Объявление Функции)
+// }
+// greet();
+
+// function() {
+//   alert('Hello World!');   (анонимная функция без имени)
+// }
+// greet();
+    
+// let greet = function () {
+//     alert('Hello World!');  Function Expression (Функциональное Выражение)
+// }
+// greet();
+
+// let getRectArea = (width, height) => width * height; (стрелочная функция)
+// console.log(getRectArea(15, 5));
+   
+// function sum() {
+//     alert(arguments.length);
+// }
+// sum(1, 2, 3, 4, 5);
+
+// function num(a, b) {
+//     if (a < b) {
+//        return (-1);
+//      } else if (a > b) {
+//         return (1);
+//     } else {
+//        return (0);
+//     }
+// }
+// console.log(num(2, 3));
+
+
+// function factorial(n) {
+//     if (n != 1) {
+//       return  n * factorial(n - 1);     //(3! = 3 * 2 * 1 = 6)
+//      } else {
+//        return (1);
+//     }
+// }
+// console.log(factorial(3));
+
+// function num(a, b, c) {
+//     return (a + b + c);
+// }
+// console.log(num('1','4','9'));
+
+// function square(a, b) {
+//   if (b==undefined){
+//     return a * a;
+//   } else {
+//     return a * b;
+//   }  
+// }
+// console.log(square(5, 2));
+// console.log(square(5));
